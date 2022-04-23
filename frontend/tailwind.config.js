873d8e5d9617e8104844f8163d315e03bbb4cb24
Diego Imbert <diegoimbert@pm.me>
2022-04-23 23:34:09 +0200

    Setup nextjs

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      midnight: { 1: '#070A14' },
+    },
+  },
+  plugins: [],
+};
